3c57ee053789cf51f57b8eeaf87f6a5360926891
Tom Marsh <ts.marsh@gmail.com>
2017-02-19 08:30:05 -0500

    wip

--- src/main/java/com/tailoredshapes/rooter/Core.java
+++ src/main/java/com/tailoredshapes/rooter/Core.java
@@ -1,12 +1,28 @@
 package com.tailoredshapes.rooter;
 
-/**
- * Created by tmarsh on 2/17/17.
- */
+import com.tailoredshapes.stash.Stash;
+
+import java.util.Optional;
+
+import static com.tailoredshapes.underbar.UnderBar.optional;
 
 
 public interface Core {
 
 
-    static formMethod
+    static Optional<String> formMethod(Stash request){
+        Optional<String> formMethod = optional();
+        if(request.contains("form-params")){
+            Stash formParams = request.get("form-params");
+            if(formParams.contains("_method")){
+                formMethod = optional(formParams.get("_method"));
+            }
+        }if(!formMethod.isPresent() && request.contains("multipart-params")){
+            Stash multipartParams = request.get("multipart-params");
+            if(multipartParams.contains("_method")){
+                formMethod = optional(multipartParams.get("_method"));
+            }
+        }
+        return formMethod;
+    }
 }
